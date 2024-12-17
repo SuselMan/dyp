486ea831f731f7f643415eb2e090168b53c28c03
ipavlukhin <ipavlukhin-contractor@evernote.com>
2024-12-17 16:58:27 +0300

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000; // порт, можно выбрать любой свободный
+const port = 80; // порт, можно выбрать любой свободный
 
 const server = http.createServer((req, res) => {
   // Определим путь к файлу
